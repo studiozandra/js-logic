be36d904512dc3202f5103c3a16182f17fee0007
studiozandra <zan@studiozandra.com>
2019-01-11 21:52:31 -1000

    add function isEqual

--- exercises.js
+++ exercises.js
@@ -133,7 +133,14 @@ console.log('bigBird ' + bigBird('the'));
  * Console.log your result.
 */
 
-
+function isEqual(first, second){
+  if(first === second){
+    return "You look mahvelous!"
+  }else{
+    return "I don't know who you are anymore."
+  }
+}
+console.log('isEqual ' + isEqual('happy', 'nappy'));
 /*
  * #7
  * Function - notEqual
